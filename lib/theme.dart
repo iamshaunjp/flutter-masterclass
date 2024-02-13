59955f36f95909231cbe92383eccc3c2d7b9ef43
Shaun <shaun@thenetninja.co.uk>
2024-02-13 10:13:56 +0000

    lesson-23-half

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primaryColor = const Color.fromARGB(255, 162, 29, 19);
-  static Color primaryAccent = const Color.fromARGB(255, 120, 14, 14);
-  static Color secondaryColor = const Color.fromARGB(255, 45, 45, 45);
-  static Color secondaryAccent = const Color.fromARGB(255, 35, 35, 35);
-  static Color titleColor = const Color.fromARGB(255, 200, 200, 200);
-  static Color textColor = const Color.fromARGB(255, 150, 150, 150);
-  static Color successColor = const Color.fromARGB(255, 9, 149, 110);
-  static Color highlightColor = const Color.fromARGB(255, 212, 172, 13);
+  static Color primaryColor = const Color.fromRGBO(162, 29, 19, 1);
+  static Color primaryAccent = const Color.fromRGBO(120, 14, 14, 1);
+  static Color secondaryColor = const Color.fromRGBO(45, 45, 45, 1);
+  static Color secondaryAccent = const Color.fromRGBO(35, 35, 35, 1);
+  static Color titleColor = const Color.fromRGBO(200, 200, 200, 1);
+  static Color textColor = const Color.fromRGBO(150, 150, 150, 1);
+  static Color successColor = const Color.fromRGBO(9, 149, 110, 1);
+  static Color highlightColor = const Color.fromRGBO(212, 172, 13, 1);
 }
 
-ColorScheme _scheme = ColorScheme.fromSeed(
-    seedColor: AppColors.primaryColor,
-);
-
-ThemeData primaryTheme = ThemeData().copyWith(
+ThemeData primaryTheme = ThemeData(
 
   // seed color theme
-  colorScheme: _scheme,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: AppColors.primaryColor,
+  ),
 
   // scaffold color
   scaffoldBackgroundColor: AppColors.secondaryAccent,
 
   // app bar theme colors
-  appBarTheme: const AppBarTheme().copyWith(
+  appBarTheme: AppBarTheme(
     backgroundColor: AppColors.secondaryColor,
     foregroundColor: AppColors.textColor,
     surfaceTintColor: Colors.transparent,
@@ -32,7 +30,7 @@ ThemeData primaryTheme = ThemeData().copyWith(
   ),
 
   // text theme
-  textTheme: const TextTheme().copyWith(
+  textTheme: TextTheme(
     bodyMedium: TextStyle(
       color: AppColors.textColor,
       fontSize: 16,
