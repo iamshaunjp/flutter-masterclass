eec278b1aae294ed4449ece8533f9374b5e865fd
Shaun <shaun@thenetninja.co.uk>
2024-02-13 15:17:47 +0000

    lesson-17

--- lib/screens/home/character_card.dart
+++ lib/screens/home/character_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_masterclass/theme.dart';
 
 class CharacterCard extends StatelessWidget {
   const CharacterCard(this.character, {super.key});
@@ -13,6 +14,17 @@ class CharacterCard extends StatelessWidget {
         child: Row(
           children: [
             Text(character),
+
+            const Expanded(
+              child: SizedBox(),
+            ),
+            
+            IconButton(
+              onPressed: () {
+                // navigate to character profile screen
+              },
+              icon: Icon(Icons.arrow_forward, color: AppColors.textColor)
+            ),
           ]
         ),
       ),
