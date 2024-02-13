78ed3b2369f2f765fd7dee6c8d035beaf622a493
Shaun <shaun@thenetninja.co.uk>
2024-02-13 13:51:26 +0000

    lesson-23

--- lib/shared/styled_text.dart
+++ lib/shared/styled_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StyledText extends StatelessWidget {
   const StyledText(this.text, {super.key});
@@ -7,7 +8,9 @@ class StyledText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: Theme.of(context).textTheme.bodyMedium);
+    return Text(text, style: GoogleFonts.kanit(
+      textStyle: Theme.of(context).textTheme.bodyMedium,
+    ));
   }
 }
 
@@ -18,7 +21,9 @@ class StyledHeading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: Theme.of(context).textTheme.headlineMedium);
+    return Text(text.toUpperCase(), style: GoogleFonts.kanit(
+      textStyle: Theme.of(context).textTheme.headlineMedium
+    ));
   }
 }
 
@@ -29,6 +34,8 @@ class StyledTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: Theme.of(context).textTheme.titleMedium);
+    return Text(text.toUpperCase(), style: GoogleFonts.kanit(
+      textStyle: Theme.of(context).textTheme.titleMedium
+    ));
   }
 }
