30159eaf5fac523ac787c345d07a00c62bfc53dc
Shaun <shaun@thenetninja.co.uk>
2024-02-13 09:44:20 +0000

    lesson-20

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primaryColor = const Color.fromARGB(255, 162, 29, 19);
-  static Color primaryAccent = const Color.fromARGB(255, 120, 14, 14);
-  static Color secondaryColor = const Color.fromARGB(255, 45, 45, 45);
-  static Color secondaryAccent = const Color.fromARGB(255, 35, 35, 35);
-  static Color titleColor = const Color.fromARGB(255, 200, 200, 200);
-  static Color textColor = const Color.fromARGB(255, 150, 150, 150);
-  static Color successColor = const Color.fromARGB(255, 9, 149, 110);
-  static Color highlightColor = const Color.fromARGB(255, 212, 172, 13);
+  static Color primaryColor = const Color.fromRGBO(162, 29, 19, 1);
+  static Color primaryAccent = const Color.fromRGBO(120, 14, 14, 1);
+  static Color secondaryColor = const Color.fromRGBO(45, 45, 45, 1);
+  static Color secondaryAccent = const Color.fromRGBO(35, 35, 35, 1);
+  static Color titleColor = const Color.fromRGBO(200, 200, 200, 1);
+  static Color textColor = const Color.fromRGBO(150, 150, 150, 1);
+  static Color successColor = const Color.fromRGBO(9, 149, 110, 1);
+  static Color highlightColor = const Color.fromRGBO(212, 172, 13, 1);
 }
 
 ColorScheme _scheme = ColorScheme.fromSeed(
