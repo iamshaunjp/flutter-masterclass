caa20ad0b84e3d23b9b92594b7dbc00e2d97fa46
Shaun <shaun@thenetninja.co.uk>
2024-02-18 13:46:23 +0000

    lesson-34

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -31,3 +31,12 @@ class Character with Stats {
   }
 
 }
+
+// dummy character data
+
+List<Character> characters = [
+  Character(id: '1', name: 'Klara', vocation: Vocation.wizard, slogan: 'Kapumf!'),
+  Character(id: '2', name: 'Jonny', vocation: Vocation.junkie, slogan: 'Light me up...'),
+  Character(id: '3', name: 'Crimson', vocation: Vocation.raider, slogan: 'Fire in the hole!'),
+  Character(id: '4', name: 'Shaun', vocation: Vocation.ninja, slogan: 'Alright then gang.'),
+];
