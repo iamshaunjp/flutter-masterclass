be063713c9d81e79de370dda24e0b8d018ff3cfe
Shaun <shaun@thenetninja.co.uk>
2024-02-21 10:15:28 +0000

    lesson-37

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masterclass/shared/styled_text.dart';
 import 'package:flutter_masterclass/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Create extends StatefulWidget {
   const Create({super.key});
@@ -32,6 +33,31 @@ class _CreateState extends State<Create> {
               child: StyledText('Create a name & slogan for your character.'),
             ),
             const SizedBox(height: 30),
+
+            // input for name & slogan
+            TextField(
+              cursorColor: AppColors.textColor,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.person_2),
+                label: StyledText('Character name'),
+              ),
+              style: GoogleFonts.kanit(
+                textStyle: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              cursorColor: AppColors.textColor,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.chat),
+                label: StyledText('Character slogan'),
+              ),
+              style: GoogleFonts.kanit(
+                textStyle: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextField(),
 
           ],
         ),
--- lib/theme.dart
+++ lib/theme.dart
@@ -59,4 +59,13 @@ ThemeData primaryTheme = ThemeData(
     margin: const EdgeInsets.only(bottom: 16),
   ),
 
+  // input decoration theme
+  inputDecorationTheme: InputDecorationTheme(
+    filled: true,
+    fillColor: AppColors.secondaryColor.withOpacity(0.5),
+    border: InputBorder.none, 
+    labelStyle: TextStyle(color: AppColors.textColor),
+    prefixIconColor: AppColors.textColor,
+  ),
+
 );
