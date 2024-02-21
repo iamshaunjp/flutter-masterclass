b13647f81f8c70e27edfb3cfcd5b2e491e721934
Shaun <shaun@thenetninja.co.uk>
2024-02-21 11:09:41 +0000

    lesson-42

--- lib/screens/create/vocation_card.dart
+++ lib/screens/create/vocation_card.dart
@@ -12,31 +12,36 @@ class VocationCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-    
-            // vocation img
-            Image.asset('assets/img/vocations/${vocation.image}', 
-              width: 80,
-              colorBlendMode: BlendMode.color,
-            ),
-            const SizedBox(width: 10),
-    
-            // vocation name & description
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  StyledHeading(vocation.title),
-                  StyledText(vocation.description)
-                ]
+    return GestureDetector(
+      onTap: () {
+        print(vocation.title);
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+      
+              // vocation img
+              Image.asset('assets/img/vocations/${vocation.image}', 
+                width: 80,
+                colorBlendMode: BlendMode.color,
               ),
-            ),
-    
-          ]
+              const SizedBox(width: 10),
+      
+              // vocation name & description
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    StyledHeading(vocation.title),
+                    StyledText(vocation.description)
+                  ]
+                ),
+              ),
+      
+            ]
+          ),
         ),
       ),
     );
