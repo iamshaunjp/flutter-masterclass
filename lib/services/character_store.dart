e05bdd558dca4625197298fafdbe5254e0995745
Shaun <shaun@thenetninja.co.uk>
2024-02-27 10:55:05 +0000

    lesson-60

---
+++ lib/services/character_store.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masterclass/models/character.dart';
+import 'package:flutter_masterclass/models/vocation.dart';
+
+class CharacterStore extends ChangeNotifier {
+
+  final List<Character> _characters = [
+    Character(id: '1', name: 'Klara', vocation: Vocation.wizard, slogan: 'Kapumf!'),
+    Character(id: '2', name: 'Jonny', vocation: Vocation.junkie, slogan: 'Light me up...'),
+    Character(id: '3', name: 'Crimson', vocation: Vocation.raider, slogan: 'Fire in the hole!'),
+    Character(id: '4', name: 'Shaun', vocation: Vocation.ninja, slogan: 'Alright then gang.'),
+  ];
+
+  get characters => _characters;
+
+}
