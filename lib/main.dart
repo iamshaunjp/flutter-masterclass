73968a8779f752a5a7a5ccdf4135245bfe6c7628
Shaun <shaun@thenetninja.co.uk>
2024-02-28 11:21:55 +0000

    lesson-64

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,17 @@ import 'package:flutter_masterclass/services/character_store.dart';
 import 'package:flutter_masterclass/theme.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+// firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(ChangeNotifierProvider(
     create: (context) => CharacterStore(),
     child: MaterialApp(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
