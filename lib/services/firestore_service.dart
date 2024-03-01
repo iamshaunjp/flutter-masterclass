7c04a64b42b63e179557cfa129e7a8d6dcfb7eb6
Shaun <shaun@thenetninja.co.uk>
2024-03-01 10:56:39 +0000

    lesson-70

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,8 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_masterclass/models/character.dart';
 
 class FirestoreService {
 
   static final ref = FirebaseFirestore.instance
-    .collection('characters');
+    .collection('characters')
+    .withConverter(
+      fromFirestore: Character.fromFirestore, 
+      toFirestore: (Character c, _) => c.toFirestore()
+    );
 
 }
