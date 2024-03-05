fa89b27fa0b580e204da9ac18e1eb26ae4ab7d03
Shaun <shaun@thenetninja.co.uk>
2024-03-05 12:28:13 +0000

    lesson-78

--- lib/screens/profile/stats_table.dart
+++ lib/screens/profile/stats_table.dart
@@ -14,6 +14,8 @@ class StatsTable extends StatefulWidget {
 
 class _StatsTableState extends State<StatsTable> {
 
+  double turns = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -27,8 +29,12 @@ class _StatsTableState extends State<StatsTable> {
             padding: const EdgeInsets.all(8),
             child: Row(
               children: [
-                Icon(Icons.star,
-                  color: widget.character.points > 0 ? Colors.yellow : Colors.grey,
+                AnimatedRotation(
+                  turns: turns,
+                  duration: const Duration(milliseconds: 200),
+                  child: Icon(Icons.star,
+                    color: widget.character.points > 0 ? Colors.yellow : Colors.grey,
+                  ),
                 ),
                 const SizedBox(width: 20),
                 const StyledText('Stat points available:'),
@@ -71,6 +77,7 @@ class _StatsTableState extends State<StatsTable> {
                       onPressed: () {
                         setState(() {
                           widget.character.increaseStat(stat['title']!);
+                          turns += 0.5;
                         });
                       },
                     ),
@@ -84,6 +91,7 @@ class _StatsTableState extends State<StatsTable> {
                       onTap: () {
                         setState(() {
                           widget.character.decreaseStat(stat['title']!);
+                          turns -= 0.5;
                         });
                       },
                     ),
